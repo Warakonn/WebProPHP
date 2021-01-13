ffbe235fed2248367a2609db19b5efabd1d0f364
Your Name <you@example.com>
2021-01-13 11:03:54 +0700

    update Lab4_1.php

--- Lab4_1.php
+++ Lab4_1.php
@@ -8,7 +8,7 @@ $z = 300;
 $add = $x + $y;
 $sub = $z - $y;
 $mul = $y * $z;
-$di = $mul / 300;
+$di = $mul / $z;
 $message = "Hello World";
 echo $x; echo "<br />";
 echo $y; echo "<br />";
@@ -19,7 +19,7 @@ echo $message; echo "<br />";
 echo $x.'+'.$y.''.'='.$add; echo "<br >";
 echo $z. '-' .$y.''.'='.$sub; echo "<br />";
 echo $y. '*' .$z.''.'='.$mul; echo "<br />";
-echo $mul. '/' .'300'.''.'='.$di; echo "<br />";
+echo $mul. '/' .$z.''.'='.$di; echo "<br />";
 
 ?>
 </body>
